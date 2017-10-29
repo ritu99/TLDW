430a1d7974adfaa6b11869b712c77d666a32ecb0
Ritvik Annam <ritu6ritu@gmail.com>
2017-10-28 20:04:52 -0500

    api keys

---
+++ msCaptions.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const request = require("request");
+
+app.post("/convert_video", (req, res) => 
+	{
+		headers = {key = ms_api_key_primary
+		
+
