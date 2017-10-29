a2823784405074665b40429a29fe33729bcb9f61
Ritvik Annam <ritu6ritu@gmail.com>
2017-10-28 23:00:54 -0500

    msCaptions.js

--- index.js
+++ index.js
@@ -4,8 +4,3 @@ const app = express();
 const videoConverter = require('./videoURL.js');
 
 app.use(bodyParser.urlencoded({extended: true}));
-
-app.listen(3000, function() {
-	console.log('listening on 3000');
-	videoConverter.convert('https://www.youtube.com/watch?v=I30cRCjd6es&app=desktop')
-})
